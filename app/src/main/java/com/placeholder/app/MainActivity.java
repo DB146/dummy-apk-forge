ec41ecd70573753cad16000934b79f98252fc473
DB146 <tranhieu06042008@gmail.com>
2026-02-19 14:49:37 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/placeholder/app/MainActivity.java
@@ -0,0 +1,11 @@
+package com.placeholder.app; // Fixed package name
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
